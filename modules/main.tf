3a42c9203f25a269f8acfa28c4009078ccd136a0
Rajesh Alli <arajesh.alli@gmail.com>
2022-10-04 11:38:59 +0530

    a

--- modules/main.tf
+++ modules/main.tf
@@ -14,38 +14,25 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-
-locals {
-  name   = "ex-${replace(basename(path.cwd), "_", "-")}"
-  region = "eu-west-1"
-
-  tags = {
-    Example    = local.name
-  }
-}
+# vpc
 
 module "vpc" {
-  source = "../../"
+  source = "terraform-aws-modules/vpc/aws"
 
-  name = local.name
-  cidr = "10.0.0.0/16"
+  name = "my-vpc"
+  cidr = "172.31.0.0/16"
 
-  azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = ["${local.region}a", "${local.region}b"]
+  private_subnets = ["172.31.0.0/24", "172.31.0.18/24"]
+  public_subnets  = ["172.31.0.16/24", "172.31.0.48/24"]
 
-  enable_ipv6 = true
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
 
-  enable_nat_gateway = false
-  single_nat_gateway = true
-
-  public_subnet_tags = {
-    Name = "overridden-name-public"
-  }
-
-  tags = local.tags
-
-  vpc_tags = {
-    Name = "vpc-name"
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
   }
 }
+
+
