88cc5a5b516c7ff74abe5e6175ecaa6a713e81ef
Rajesh Alli <arajesh.alli@gmail.com>
2022-10-04 14:24:04 +0530

    e

---
+++ efs/main.tf
@@ -0,0 +1,25 @@
+# aws providers terraform
+
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_efs_file_system" "efs" {
+   creation_token = "efs"
+   performance_mode = "generalPurpose"
+   throughput_mode = "bursting"
+   encrypted = "true"
+ tags = {
+     Name = "EFS"
+   }
+ }
