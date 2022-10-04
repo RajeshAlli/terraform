6febda22886cc8ffea65c1126927895dc236b120
Rajesh Alli <arajesh.alli@gmail.com>
2022-10-04 14:46:18 +0530

    a

--- efs/main.tf
+++ efs/main.tf
@@ -23,3 +23,19 @@ resource "aws_efs_file_system" "efs" {
      Name = "EFS"
    }
  }
+
+ resource "aws_launch_configuration" "sample" {
+  image_id          = "ami-04afe279c8bff9ed8"
+  instance_type = "t2.micro"
+  security_groups = [
+    aws_security_group.efs.id,
+  ]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${module.efs_mount.file_system_dns_name}:/ /your/mount/point/
+              EOF
+  lifecycle {
+    create_before_destroy = true
+  }
+}
