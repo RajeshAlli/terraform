4795664e30ab4772c794edfc808e3ddf494f6bbf
Rajesh Alli <arajesh.alli@gmail.com>
2022-10-08 12:01:27 +0530

    a

--- efs/main.tf
+++ efs/main.tf
@@ -15,3 +15,15 @@ resource "aws_efs_mount_target" "efs-mt" {
    subnet_id = aws_subnet.subnet[count.index].id
    security_groups = [aws_security_group.efs.id]
  }
+
+ resource "aws_instance" "testinstance" {
+    ami = "ami-087c17d1fe0178315"
+    instance_type = "t2.micro"
+    subnet_id = aws_subnet.subnet[0].id
+    associate_public_ip_address= true
+    vpc_security_group_ids = [ aws_security_group.ec2.id ]
+    key_name="efs"
+    tags= {
+        Name = "testinstance"
+    }
+}
