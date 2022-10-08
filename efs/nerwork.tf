c9f88caea5567ddc46e273cc1e83ca06129f493a
Rajesh Alli <arajesh.alli@gmail.com>
2022-10-08 13:45:08 +0530

    a

--- efs/nerwork.tf
+++ efs/nerwork.tf
@@ -1,5 +1,6 @@
-##data "aws_availability_zones" "available" {}
 
+
+data "aws_availability_zones" "available" {}
 resource "aws_vpc" "vpc" {
    cidr_block = "10.0.0.0/16"
    enable_dns_hostnames = true
