efa284c8ea34310a1a24ef8513882b5b80218dd0
Rajesh Alli <arajesh.alli@gmail.com>
2022-10-08 15:43:53 +0530

    A

--- modules/vpc-subnets.tf
+++ modules/vpc-subnets.tf
@@ -23,8 +23,8 @@ module "vpc" {
   cidr = "0.0.0.0/0"
 
   azs             = ["us-east-1a"]
-  private_subnets = ["192.0.0.0/25"]
-  public_subnets  = ["192.0.0.128/25"]
+  private_subnets = ["192.0.0.0/16"]
+  public_subnets  = ["192.0.0.128/28"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
