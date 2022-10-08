33b46499853cc17892395c3fda2e1e5824bb5160
Rajesh Alli <arajesh.alli@gmail.com>
2022-10-08 11:10:34 +0530

    a

--- efs/main.tf
+++ efs/main.tf
@@ -35,3 +35,31 @@ resource "aws_vpc" "my_vpc-efs" {
   }
 }
 
+#### aws subnets public-sub and private-sub
+
+
+resource "aws_subnet" "public-sub" {
+  vpc_id     = aws_vpc.my-vpc-efs.id
+  cidr_block = "172.31.0.0/28"
+  availability_zone = "us-east-1a"
+  enable_resource_name_dns_a_record_on_launch="true"
+  map_public_ip_on_launch = "true"
+  tags = merge(
+    local.tags,
+    {
+      Name = "public-sub-efs"
+    })
+}
+
+
+resource "aws_subnet" "private-sub" {
+  vpc_id     = aws_vpc.my-vpc-efs.id
+  cidr_block = "172.31.0.16/28"
+  availability_zone = "us-east-1b"
+  enable_resource_name_dns_a_record_on_launch="true"
+  tags = merge(
+    local.tags,
+    {
+      Name = "private-sub-efs"
+    })
+}
