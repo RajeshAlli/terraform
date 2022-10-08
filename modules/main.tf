39baacccd66499664e934064dfb8c103e757f9c7
Rajesh Alli <arajesh.alli@gmail.com>
2022-10-08 14:54:55 +0530

    a

--- modules/main.tf
+++ modules/main.tf
@@ -14,17 +14,17 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-# vpc
+# created vpc
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "my-vpc"
-  cidr = "172.31.0.0/18"
+  cidr = "192.0.0.0/24"
 
   azs             = ["us-east-1a", "us-east-1b"]
-  private_subnets = ["172.31.1.0/24", "172.31.2.0/24"]
-  public_subnets  = ["172.31.101.0/24", "172.31.102.0/24"]
+  private_subnets = ["192.0.0.0/25", "192.0.0.16/25"]
+  public_subnets  = ["192.0.0.32/25", "192.0.0.128/25"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
