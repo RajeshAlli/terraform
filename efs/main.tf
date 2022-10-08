4428641a448abd31dd653c2ca4cd8acd0ce7c715
Rajesh Alli <arajesh.alli@gmail.com>
2022-10-08 12:08:52 +0530

    a

--- efs/main.tf
+++ efs/main.tf
@@ -28,6 +28,28 @@ resource "aws_efs_mount_target" "efs-mt" {
     }
 }
 
+
+## terraform-key-pair ssh-keygen  generated_key
+
+resource "tls_private_key" "dev_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = var.generated_key_name
+  public_key = tls_private_key.dev_key.public_key_openssh
+
+  provisioner "local-exec" {    # Generate "terraform-key-pair.pem" in current directory
+    command = <<-EOT
+      echo '${tls_private_key.dev_key.private_key_pem}' > ./'${var.generated_key_name}'.pem
+      chmod 400 ./'${var.generated_key_name}'.pem
+    EOT
+  }
+
+}
+
+
 data "aws_availability_zones" "available" {}
 
 resource "aws_vpc" "vpc" {
