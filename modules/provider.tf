2dd29eb415e0867bc0b4c6ded74b6fa64542beab
Rajesh Alli <arajesh.alli@gmail.com>
2022-10-09 10:27:55 +0530

    a

---
+++ modules/provider.tf
@@ -0,0 +1,15 @@
+# aws providers terraform
+
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
--- modules/vpc-subnets.tf
+++ modules/vpc-subnets.tf
@@ -1,18 +1,3 @@
-# aws providers terraform
-
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
-provider "aws" {
-  region  = "us-east-1"
-}
 
 # created vpc
 
