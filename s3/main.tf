935f9f8e6c2a3a1c4663d27e8e148cdc27394de7
Rajesh Alli <arajesh.alli@gmail.com>
2022-10-09 18:18:38 +0530

    we3

---
+++ s3/main.tf
@@ -0,0 +1,13 @@
+# create "s3 bucknet"
+
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-s3-bucket"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
+}
