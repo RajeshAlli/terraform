9b535eded8f43599ee20aa67aecad4fe1b1417b7
Rajesh Alli <arajesh.alli@gmail.com>
2022-10-09 10:41:15 +0530

    a

---
+++ modules/ec2.tf
@@ -0,0 +1,19 @@
+# create "ec2 instance"
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "module-instance"
+
+  ami                    = "ami-087c17d1fe0178315"
+  instance_type          = "t2.micro"
+  key_name               = "module-key"
+  monitoring             = true
+  vpc_security_group_ids = ["aws_security_group.ec2.id"]
+  subnet_id              = "aws_subnet.subnet[0].id"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
